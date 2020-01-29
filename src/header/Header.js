20bda1b430c934669127306773fc9f0cf328a693
Pratik Gurung <pgurung0610@gmail.com>
2020-01-29 01:16:31 -0500

    Made header a link to home page

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => (
     <header>
-        <h1>Film Find</h1>
+        <Link to={'/'} className="title">
+            <h1>Film Find</h1>
+        </Link>
     </header>
 )
 
